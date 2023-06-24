526a66bd696d88080578ab8af67f0b5c7b56ac19
hj <49115022+Hjain518@users.noreply.github.com>
2023-06-25 01:05:55 +0530

    Intro Changes

--- react-deploy/src/components/body/about/index.js
+++ react-deploy/src/components/body/about/index.js
@@ -7,8 +7,12 @@ function About() {
         <div className="about">
            <div className="about-top">
             <div className="about-info">
-            Hello There 👋, I am <br/> <span className="info-name">Harshit Jain</span>. 
-            <br/>I love experimenting with the web and writing about it !
+             1. <span className="info-name"> Software Developer</span>
+            <br/>
+            2. <span className="info-name"> UI/UX Designer</span>
+            <br/>
+            3. <span className="info-name">Technical Writer</span>
+            <br/>
             </div>
             <div className="about-photo">
               {/* image not loading because you can use create react app so it is webpack, so webpack needs some extra parameter to load the image of local, so we use .default to load the local image on localhost and browsers */}
